7ae963ca67ff0c32c79c9023b59b525402786f81
dima320 <dmytrosvyshch@gmail.com>
2026-07-24 00:55:50 +0300

    Finalize services, team, and contacts

--- assets/js/config.js
+++ assets/js/config.js
@@ -12,13 +12,15 @@ const SITE_CONFIG = {
   bookingUrl: "https://closedspace.easyweek.com.ua/",
 
   // Контакти
+  phone: "+380734716407",
+  phoneDisplay: "+380 73 471 6407",
   // Адреса
   address: "вул. Минайська 18Г, Ужгород",
   mapEmbedUrl: "https://www.google.com/maps?q=%D0%B2%D1%83%D0%BB.%20%D0%9C%D0%B8%D0%BD%D0%B0%D0%B9%D1%81%D1%8C%D0%BA%D0%B0%2018%D0%93,%20%D0%A3%D0%B6%D0%B3%D0%BE%D1%80%D0%BE%D0%B4&output=embed",
   mapLink: "https://maps.app.goo.gl/pgFpMge8N1pjW66ZA",
 
   // Години роботи
-  workingHours: "Пн–Вт, Чт–Сб: 10:00 – 19:30; Ср, Нд: Вихідний",
+  workingHours: "Пн–Сб: 10:00 – 19:00; Нд: Вихідний",
 
   // Соцмережі
   instagram: "https://www.instagram.com/closedspace_barbershop",
